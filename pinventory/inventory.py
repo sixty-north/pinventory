e126132c865404fa5ccb47ca01da86354652cd3b
Robert Smallshire <robert@smallshire.org.uk>
2017-01-25 10:17:53 +0100

    Skeletal dynamic inventory script which looks for Raspberry Pi devices on the primary local network.

--- pinventory/inventory.py
+++ pinventory/inventory.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Pinventory.
 
-A dynamic inventory provider for Ansible that finds fresh Raspberry Pis.
+A dynamic inventory provider for Ansible that finds fresh Raspberry Pi devices.
 
 Usage:
     pinventory --list
@@ -15,14 +15,41 @@ Options:
 
 """
 import json
+import socket
 import sys
+
 from docopt import docopt
 
 from pinventory import __version__
+from pinventory.network import arp_ip_mac, establish_routes
+
+RASPBERRY_PI_MAC_ADDRESS_PREFIX = 'b8:27:eb:'
+
+
+def locate_pi_devices():
+    """Find all Raspberry Pi devices on the local network.
+
+    Raspberry Pi computers are identified by the first three
+    bytes of their MAC address.
+    """
+    establish_routes()
+    pi_ip_hostnames = ip_hostnames_matching_mac_prefix(RASPBERRY_PI_MAC_ADDRESS_PREFIX)
+    return pi_ip_hostnames
+
+
+def ip_hostnames_matching_mac_prefix(mac_prefix):
+    pi_ip_addresses = [ip for ip, mac in arp_ip_mac() if mac.startswith(mac_prefix)]
+    ip_hostnames = []
+    for pi_ip_address in pi_ip_addresses:
+        hostaddr, _, _, = socket.gethostbyaddr(pi_ip_address)
+        hostname, _, _ = hostaddr.partition('.')
+        ip_hostnames.append((pi_ip_address, hostname))
+    return ip_hostnames
 
 
 def make_inventory():
-    return {}
+    pi_ip_hostnames = locate_pi_devices()
+    return dict(pi_ip_hostnames)
 
 
 def collect_host_variables(hostname):
@@ -40,6 +67,7 @@ def print_host_variables(hostname):
     host_variables = collect_host_variables(hostname)
     json_host_variables = json.dumps(host_variables)
     print(json_host_variables)
+
 
 def main(argv=None):
     arguments = docopt(__doc__, argv, version=__version__)
---
+++ pinventory/network.py
@@ -0,0 +1,148 @@
+import sys
+import socket
+import ipaddress
+import subprocess
+
+
+_local_networks = ("127.", "0:0:0:0:0:0:0:1")
+
+# ignore these prefixes -- localhost, unspecified, and link-local
+_ignored_networks = _local_networks + ("0.", "0:0:0:0:0:0:0:0", "169.254.", "fe80:")
+
+
+def detect_family(addr):
+    if "." in addr:
+        assert ":" not in addr
+        return socket.AF_INET
+    elif ":" in addr:
+        return socket.AF_INET6
+    else:
+        raise ValueError("invalid ipv4/6 address: %r" % addr)
+
+
+def expand_addr(addr):
+    """convert address into canonical expanded form --
+    no leading zeroes in groups, and for ipv6: lowercase hex, no collapsed groups.
+    """
+    family = detect_family(addr)
+    addr = socket.inet_ntop(family, socket.inet_pton(family, addr))
+    if "::" in addr:
+        count = 8-addr.count(":")
+        addr = addr.replace("::", (":0" * count) + ":")
+        if addr.startswith(":"):
+            addr = "0" + addr
+    return addr
+
+
+def _get_local_addr(family, remote):
+    try:
+        s = socket.socket(family, socket.SOCK_DGRAM)
+        try:
+            s.connect((remote, 9))
+            return s.getsockname()[0]
+        finally:
+            s.close()
+    except socket.error:
+        return None
+
+
+def get_local_addr(remote=None, ipv6=True):
+    """Get primary LAN address of host
+
+    Args:
+        remote: If supplied, return LAN address that host would use
+            to access that specific remote address. By default,
+            returns address it would use to access the public
+            internet.
+
+        ipv6: Attempt to find an ipv6 address first if True,
+            otherwise only check ipv4.
+
+    Returns:
+        Primary LAN address for host, or None if could not be
+        determined.
+    """
+    if remote:
+        family = detect_family(remote)
+        local = _get_local_addr(family, remote)
+        if not local:
+            return None
+        if family == socket.AF_INET6:
+            # expand zero groups so the startswith() test works.
+            local = expand_addr(local)
+        if local.startswith(_local_networks):
+            # border case where remote addr belongs to host
+            return local
+    else:
+        # NOTE: the two addresses used here are TESTNET addresses,
+        #       which should never exist in the real world.
+        if ipv6:
+            local = _get_local_addr(socket.AF_INET6, "2001:db8::1234")
+            # expand zero groups so the startswith() test works.
+            if local:
+                local = expand_addr(local)
+        else:
+            local = None
+        if not local:
+            local = _get_local_addr(socket.AF_INET, "192.0.2.123")
+            if not local:
+                return None
+    if local.startswith(_ignored_networks):
+        return None
+    return local
+
+
+def local_ip_address(remote=None, ipv6=True):
+    addr = get_local_addr(remote, ipv6)
+    if addr is None:
+        return None
+    return ipaddress.ip_address(addr)
+
+
+def local_netmask(local_ip_address):
+    a = subprocess.check_output("ifconfig", shell=True).decode(sys.getdefaultencoding())
+    matches = filter(lambda line: line.startswith("inet " + str(local_ip_address)),
+                   map(str.strip, a.splitlines()))
+    try:
+        match = next(matches)
+    except StopIteration:
+        raise RuntimeError("Could not locate network corresponding to {}".format(local_ip_address))
+
+    _, _, suffix = match.partition('netmask')
+    netmask_hex = suffix.split()[0]
+    netmask_int = int(netmask_hex, base=0)
+    netmask_address = ipaddress.ip_address(netmask_int)
+    return str(netmask_address)
+
+
+def make_network(ip_address, netmask):
+    ip_address = ipaddress.ip_address(ip_address)
+    netmask = ipaddress.ip_address(netmask)
+    network_address = ipaddress.ip_address(int(ip_address) & int(netmask))
+    return ipaddress.ip_network('/'.join((str(network_address), str(netmask))))
+
+
+def local_network(remote=None, ipv6=True):
+    ip_address = local_ip_address(remote, ipv6)
+    netmask = local_netmask(ip_address)
+    return make_network(ip_address, netmask)
+
+
+def arp_ip_mac():
+    a = subprocess.check_output("arp -an", shell=True).decode(sys.getdefaultencoding())
+    for line in a.splitlines():
+        words = line.split()
+        maybe_ip_address = [word[1:-1] for word in words if word.count('.') == 3]
+        maybe_mac_address = [word for word in words if word.count(':') == 5]
+        if maybe_ip_address and maybe_mac_address:
+            yield maybe_ip_address[0], maybe_mac_address[0]
+
+
+def establish_routes():
+    """Establish routes to all devices on the local network using nmap.
+
+    This is useful so that subsequent calls to arp_ip_mac() locate all devices.
+    """
+    network = local_network()
+    nmap_command = 'nmap -sP {!s}'.format(network)
+    subprocess.check_call(nmap_command, shell=True, stdout=subprocess.DEVNULL)
