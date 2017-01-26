887b8dca96d30085a1fb9b2499a4d9d8ed5ee291
Robert Smallshire <robert@smallshire.org.uk>
2017-01-26 21:24:13 +0100

    Now responds correctly to --hosts.

--- pinventory/inventory.py
+++ pinventory/inventory.py
@@ -4,14 +4,15 @@
 A dynamic inventory provider for Ansible that finds fresh Raspberry Pi devices.
 
 Usage:
-    pinventory --list
-    pinventory --host <hostname>
+    pinventory --list [--indent]
+    pinventory --host <hostname> [--indent]
 
 Options:
     -h --help  Show this screen.
     --version  Show version.
     --list     Output a JSON encoded dictionary of all groups.
     --host     Output a JSON encoded dictionary of variables for the specified host.
+    --indent   Pretty-print JSON output
 
 """
 import json
@@ -24,7 +25,7 @@ from pinventory import __version__
 from pinventory.network import arp_ip_mac, establish_routes
 
 RASPBERRY_PI_MAC_ADDRESS_PREFIX = 'b8:27:eb:'
-
+DEFAULT_JSON_INDENTATION = 4
 
 def locate_pi_devices():
     """Find all Raspberry Pi devices on the local network.
@@ -49,45 +50,54 @@ def ip_hostname_macs_matching_mac_prefix(mac_prefix):
 
 def make_inventory():
     ip_hostname_macs = locate_pi_devices()
-    print(ip_hostname_macs)
     all_raspberry_group = set(ip for ip, hostname, mac in ip_hostname_macs)
     raw_raspberry_group = set(ip for ip, hostname, mac in ip_hostname_macs if hostname.startswith('raspberrypi'))
     sol_raspberry_group = set(ip for ip, hostname, mac in ip_hostname_macs if hostname.startswith('sol-'))
     taken_raspberry_group = all_raspberry_group - raw_raspberry_group - sol_raspberry_group
-    hostvars = {ip: {'mac': mac} for ip, _, mac in ip_hostname_macs}
+    hostsvars = make_hostsvars()
     return {
         'raw-raspberries': list(raw_raspberry_group),
         'sol-raspberries': list(sol_raspberry_group),
         'taken-rasperries': list(taken_raspberry_group),
         '_meta': {
-            'hostvars': hostvars
+            'hostvars': hostsvars
         }
     }
 
 
+def make_hostsvars():
+    ip_hostname_macs = locate_pi_devices()
+    return {ip: {'mac': mac} for ip, _, mac in ip_hostname_macs}
+
+
 def collect_host_variables(hostname):
-    _ = hostname
-    return {}
+    hostsvars = make_hostsvars()
+    try:
+        hostvars = hostsvars[hostname]
+    except KeyError:
+        hostvars = {}
+    return hostvars
 
 
-def print_inventory():
+def print_inventory(indent=None):
     inventory = make_inventory()
-    json_inventory = json.dumps(inventory)
+    json_inventory = json.dumps(inventory, indent=indent, sort_keys=True)
     print(json_inventory)
 
 
-def print_host_variables(hostname):
+def print_host_variables(hostname, indent=None):
     host_variables = collect_host_variables(hostname)
-    json_host_variables = json.dumps(host_variables)
+    json_host_variables = json.dumps(host_variables, indent=indent, sort_keys=True)
     print(json_host_variables)
 
 
 def main(argv=None):
     arguments = docopt(__doc__, argv, version=__version__)
+    indent = DEFAULT_JSON_INDENTATION if arguments['--indent'] else None
     if arguments['--list']:
-        print_inventory()
+        print_inventory(indent=indent)
     elif arguments['--host']:
-        print_host_variables(arguments['<hostname>'])
+        print_host_variables(arguments['<hostname>'], indent=indent)
 
 if __name__ == '__main__':
     sys.exit(main())
